4565a93805d760805038b364c16aeae3e2e4b7ab
Inamulhaq Inamdar <inam12507@gmail.com>
2023-09-21 15:57:19 +0530

    admin folder added

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://api.finiinfotech.com",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f94c57e05bf3a64b1d9bf5c7eed5f260'),
+      secret: env("ADMIN_JWT_SECRET", "f94c57e05bf3a64b1d9bf5c7eed5f260"),
     },
   },
 });
