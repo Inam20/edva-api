df7a557f681da371cfc872e15558f4daf29eee9c
Inamulhaq Inamdar <inam12507@gmail.com>
2023-09-29 18:02:31 +0530

    cloudinary added env

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: "dcoivjy3a",
-      api_key: "333531862236948",
-      api_secret: "NfxvLQKu9N4U3dLEWRIGJShR7TI",
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
   },
 });
