d6c6109a2151140b6209dc91a0a1170e9d8e1e52
Inamulhaq Inamdar <inam12507@gmail.com>
2023-10-23 15:46:21 +0530

    minner changes

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://api.finiinfotech.com",
+  // url: "http://api.finiinfotech.com",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "f94c57e05bf3a64b1d9bf5c7eed5f260"),
